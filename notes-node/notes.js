a1d6469bb1d6b40c90624d500d632f242dba0860
milosodalovic <oddomir@hotmail.com>
2018-01-07 14:33:42 +0100

    first commit

---
+++ notes-node/notes.js
@@ -0,0 +1,58 @@
+console.log( 'Starting notes.js' );
+
+const fs = require( 'fs' )
+
+var fetchNotes = ()=> {
+    try {
+        var notesString = fs.readFileSync( 'notes-data.json' )
+        return JSON.parse( notesString )
+    } catch ( e ) {
+        return []
+    }
+}
+
+var saveNotes = ( notes ) => {
+    fs.writeFileSync( 'notes-data.json', JSON.stringify( notes ) )
+}
+
+var addNote = ( title, body ) => {
+    var notes = fetchNotes()
+    var note  = {
+        title, body
+    }
+
+    var duplicates = notes.filter( note => note.title === title )
+
+    if ( duplicates.length === 0 ) {
+        notes.push( note )
+        saveNotes( notes )
+        return note
+    }
+
+}
+
+var getAll = () => {
+    console.log( 'getAll' );
+}
+
+var getNote = ( title ) => {
+    var notes = fetchNotes()
+
+    return notes.find(note=>note.title === title)
+}
+
+var removeNote = ( title ) => {
+    var notes = fetchNotes()
+    var filteredNotes = notes.filter( note => note.title !== title )
+    saveNotes(filteredNotes)
+    return notes.length !== filteredNotes.length
+}
+
+var logNote = (note) => {
+    console.log( '---' );
+    console.log( `Title: ${note.title}` );
+    console.log( `Body: ${note.body}` );
+}
+module.exports = {
+    addNote, getAll, getNote, removeNote, logNote
+}
